f396a2260886e31eebf034e2123713fc41857970
25-do <erickimosavej@gmail.com>
2025-03-01 19:35:16 +0300

    surport of other file types and concurrency

--- controllers/convert_controller.go
+++ controllers/convert_controller.go
@@ -3,9 +3,9 @@ package controllers
 import (
 	"net/http"
 
-	"image-converter/services"
-
 	"github.com/gin-gonic/gin"
+	"github.com/h2non/bimg"
+	"image-converter/services"
 )
 
 type ImageController struct {
@@ -17,6 +17,34 @@ func NewImageController(service services.ImageService) *ImageController {
 }
 
 func (c *ImageController) ConvertToJPEG(ctx *gin.Context) {
+	formart := ctx.Query("format")
+	var imgFormat bimg.ImageType
+	switch formart {
+	case "webp":
+		imgFormat = bimg.WEBP
+
+	case "jpg", "jpeg":
+		imgFormat = bimg.JPEG
+
+	case "png":
+		imgFormat = bimg.PNG
+
+	case "tiff":
+		imgFormat = bimg.TIFF
+
+	case "avif":
+		imgFormat = bimg.AVIF
+
+	case "svg":
+		imgFormat = bimg.SVG
+
+	case "pdf":
+		imgFormat = bimg.PDF
+
+	default:
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid File Formart"})
+
+	}
 	form, err := ctx.MultipartForm()
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse form"})
@@ -28,7 +56,7 @@ func (c *ImageController) ConvertToJPEG(ctx *gin.Context) {
 		return
 	}
 
-	converted, err := c.service.ConvertToJPEG(files)
+	converted, err := c.service.InitializeJobs(files, imgFormat)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
--- services/convert_service.go
+++ services/convert_service.go
@@ -6,12 +6,14 @@ import (
 	"mime/multipart"
 	"os"
 	"path/filepath"
+	"runtime"
+	"sync"
 
 	"github.com/h2non/bimg"
 )
 
 type ImageService interface {
-	ConvertToJPEG(file []*multipart.FileHeader) ([]string, error)
+	InitializeJobs(file []*multipart.FileHeader, format bimg.ImageType) ([]string, error)
 }
 
 // BimgService struct implementing ImageService
@@ -22,35 +24,95 @@ func NewBimgService() ImageService {
 	return &BimgService{}
 }
 
-func (s *BimgService) ConvertToJPEG(files []*multipart.FileHeader) ([]string, error) {
-	var convertedPaths []string
+type Job struct {
+	Number int
+	File   *multipart.FileHeader
+	Format bimg.ImageType
+}
+
+func ConvertFilesWoker(jobs <-chan Job, results chan<- string, wg *sync.WaitGroup) error {
+	defer wg.Done()
 
 	// loop through the uploaded files
-	for _, file := range files {
+	for file := range jobs {
 		// open image for reading
-		src, err := file.Open()
+		src, err := file.File.Open()
 		if err != nil {
-			return nil, fmt.Errorf("failed to open file: %v", err)
+			return fmt.Errorf("failed to open file: %v", err)
 		}
 		defer src.Close()
 		// read file and conert to bytes
 		fileBytes, err := io.ReadAll(src)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		// convert the bytes to webp file formart
-		webp_image, err := bimg.NewImage(fileBytes).Convert(bimg.WEBP)
+		// convert the bytes to the specified file formart
+		webp_image, err := bimg.NewImage(fileBytes).Convert(file.Format)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 
-		output := filepath.Join("output", file.Filename+"_converted.webp")
-		if err := os.WriteFile(output, webp_image, 0o644); err != nil {
-			return nil, fmt.Errorf("failed to save image: %v", err)
+		ext := getExtension(file.Format)
+
+		outputpath := filepath.Join("output", file.File.Filename+"_converted."+ext)
+		if err := os.WriteFile(outputpath, webp_image, 0o644); err != nil {
+			return fmt.Errorf("failed to save image: %v", err)
 		}
 
-		convertedPaths = append(convertedPaths, output)
+		results <- outputpath
+
+	}
+
+	return nil
+}
+
+func (s *BimgService) InitializeJobs(images []*multipart.FileHeader, format bimg.ImageType) ([]string, error) {
+	// number of wokers in respect to your machines CPU Cores.
+	tasks := len(images)
+	numwokers := runtime.NumCPU()
+	// number of jobs to be completed i.e number of images to be converted.
+	jobs := make(chan Job, tasks)
+	results := make(chan string, tasks)
+	var wg sync.WaitGroup
+
+	// start wokers to convert images
+	for i := 0; i < numwokers; i++ {
+		wg.Add(1)
+		go ConvertFilesWoker(jobs, results, &wg)
+	}
+	// send tasks to jobs
+	for i, file := range images {
+		jobs <- Job{Number: i, File: file, Format: format}
+	}
+	close(jobs)
+	wg.Wait()
+
+	close(results)
+
+	var convertedPaths []string
+	for path := range results {
+		convertedPaths = append(convertedPaths, path)
 	}
 
 	return convertedPaths, nil
+}
+
+// getExtension maps bimg.ImageType to a file extension
+func getExtension(format bimg.ImageType) string {
+	switch format {
+	case bimg.WEBP:
+		return "webp"
+	case bimg.JPEG:
+		return "jpg"
+	case bimg.PNG:
+		return "png"
+	case bimg.TIFF:
+		return "tiff"
+	case bimg.AVIF:
+		return "avif"
+	case bimg.SVG:
+		return "svg"
+	default:
+		return "img"
+	}
 }
